11f571cc1c24569c513128c8ca2c2e886c3ae0fd
Jonathan Fok kan <jfokkan@gmail.com>
2015-08-12 18:04:49 -0400

    Feature: Started payload types in format.go

--- chess-server/src/protocol/format.go
+++ chess-server/src/protocol/format.go
@@ -5,11 +5,67 @@ import ()
 type MsgType int
 
 const (
-	ConnectionMsg = iota
-	JoinMsg
-	MoveMsg
-	ChatMsg
+	Ping = iota
+	Connect
+	ConnectionReply
+	RequestOpenGamesList
+	SendOpenGamesList
+	CreateGame
+	JoinGame
+	GamePreGameStatus
+	AcceptGame
+	RejectGame
+	GameState
+	GameMove
+	AbandonGame
+	ChatMessage
+	GenericResult
 )
 
-type Move struct {
+type PingMsg struct {
+}
+
+type ConnectMsg struct {
+}
+
+type ConnectionReplyMsg struct {
+}
+
+type RequestOpenGamesListMsg struct {
+}
+
+type SendOpenGamesListMsg struct {
+}
+
+type CreateGameMsg struct {
+}
+
+type JoinGameMsg struct {
+}
+
+type GamePreGameStatus struct {
+}
+
+type AcceptGameMsg struct {
+}
+
+type RejectGameMsg struct {
+}
+
+type GameAcceptedMsg struct {
+}
+
+type GameStateMsg struct {
+}
+
+type GameMoveMsg struct {
+}
+
+type AbandonGameMsg struct {
+}
+
+type ChatMessageMsg struct {
+}
+
+type GenericResultMsg struct {
 }
